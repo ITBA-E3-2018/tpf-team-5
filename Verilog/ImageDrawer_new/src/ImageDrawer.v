7ab1db4f13f69a9bfe743c40bf0a63c240c8ffc4
Ezequiel <evijande@itba.edu.ar>
2018-11-14 15:05:02 -0300

    Empezando el nuevo modulo de ImageDrawer

--- Verilog/ImageDrawer_new/src/ImageDrawer.v
+++ Verilog/ImageDrawer_new/src/ImageDrawer.v
@@ -22,23 +22,35 @@ module ImageDrawer(
     parameter MARGIN_SPACE =( (SCREEN_WIDTH-(DIGIT_WIDTH*39)/4)/2 );
     parameter IMAGE_WIDTH = ((9*DIGIT_WIDTH)+(DIGIT_WIDTH/4)*3); //Ancho realmente utilizado para dibujar.
     parameter IMAGE_HEIGHT = DIGIT_HEIGHT;
+    //Estados
+    parameter DRAWING_0 = 0;
+    parameter DRAWING_1 = 1;
+    parameter DRAWING_2 = 2;
+    parameter DRAWING_3 = 3;
+    parameter DRAWING_4 = 4;
+    parameter DRAWING_5 = 5;
+    parameter DRAWING_6 = 6;
+    parameter DRAWING_7 = 7;
+    parameter DRAWING_8 = 8;
+    parameter DRAWING_9 = 9;
+    parameter DRAWING_DOTS = 10;
+    parameter DRAWING_BLANK = 11;
 	 //Inputs
     input wire[3:0] hours;
     input wire[5:0] minutes;
     input wire[5:0] seconds;
     input wire[9:0] milliseconds;
-	 input enable;
-	 input [15:0]row;
-	 input [15:0]column;
+    input enable;
+    input [15:0]row;
+    input [15:0]column;
 	 //Outputs
 	 output reg r;
 	 output reg g;
 	 output reg b;
 	 //Variables utilizadas
-	 reg [((IMAGE_WIDTH*IMAGE_HEIGHT)-1):0] image; //Se guarda la imagen de la pantalla.
-    reg[15:0] pos_i; //Variable auxiliar par ver donde dibujar.
-    reg[9:0] i1;
-    reg[9:0] j1;
+	  reg[3:0] state;
+    reg[15:0] counter_row; //Registro que guarda en que pixel se esta dibujando.
+    reg[15:0] counter_col;
     reg[3:0] minutes_H;
     reg[3:0] minutes_L;
     reg[3:0] seconds_H;
@@ -48,12 +60,11 @@ module ImageDrawer(
     reg[3:0] milliseconds_L;
 
     task Draw0;
-      reg[7:0] i;
-      reg[7:0] j;
       
       begin
-        for (j=0;j<(DIGIT_HEIGHT/10) ; j=j+1) begin
-          for (i = 0; i<(DIGIT_WIDTH) ; i=i+1) begin
+          if (counter_row<= (DIGIT_WIDTH/10) ) begin
+            g <= ON;
+          end
             //Dibuja la arista superior del 0.
             image[pos_i+i+(j*IMAGE_WIDTH)]=ON;
             //Dibuja la arista inferior del 0.
@@ -69,22 +80,24 @@ module ImageDrawer(
             image[pos_i+i*IMAGE_WIDTH+(9*DIGIT_WIDTH)/10+j]=ON;
           end
         end
-        pos_i= pos_i+DIGIT_WIDTH;
+        if(counter_col < DIGIT_WIDTH)
+          counter_col = counter_col + 1;
+        else begin
+          counter_col = 0;
+          counter_row = counter_row + 1;
+        end
       end
     endtask
 
     task Draw1;
-      reg[7:0] i;
-      reg[7:0] j;
       begin
-        
         for (j=0;j<(DIGIT_WIDTH/10) ; j=j+1) begin
           for (i = 0; i<(DIGIT_HEIGHT) ; i=i+1) begin
             //Dibuja la arista derecha del 1.
             image[pos_i+(i*IMAGE_WIDTH)+(9*DIGIT_WIDTH)/10+j]=ON;
           end
         end
-        pos_i= pos_i+DIGIT_WIDTH;
+        counter = counter +1;
       end
       
     endtask
@@ -340,185 +353,138 @@ module ImageDrawer(
     endtask
 
     //La pantalla comienza toda en negro.
-    initial begin
-      for (j1 = 0; j1< IMAGE_HEIGHT; j1=j1+1 ) begin
-        for (i1 = 0; i1< IMAGE_WIDTH ; i1= i1+1 ) begin
-          image[i1+(j1*IMAGE_WIDTH)] = OFF;
-        end
-      end
-      //Inicializo el display con todos ceros.
-      pos_i= 0;
-      //pongo en 0 las horas.
-      Draw0;
-      pos_i=pos_i + (DIGIT_WIDTH/10); //dejo un espacio
-      DrawDots;
-      pos_i=pos_i + (DIGIT_WIDTH/10); //dejo un espacio
-      //Pongo en 0 los minutos.
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      DrawDots;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      //Pongo en cero los segundos
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      DrawDots;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      //Pongo en cero los milisegundos.
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Draw0;
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Draw0;
-		r=OFF;
+    initial begin 
+		 r=OFF;
 	   g=OFF;
 	   b=OFF;
     end
 	 always @(row or column) begin
 	  if (enable) begin
 		 if ( ( (row>=200)&&(row<280) )&&((column>=125)&&(column<515) ) ) begin
-			g<= image[((row-200)*IMAGE_WIDTH)+(column-125)];
+			//Se esta barriendo la zona de dibujo.
+      if (row == 200) begin //switch donde se actualiza el estado actual
+        case (column)
+          125:
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= hours; //pixel en el que empiezan las horas.
+          end
+           
+          (125+DIGIT_WIDTH):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= DRAWING_DOTS; //pixel en el que empiezan los primeros puntos.
+          end
+
+          (125+DIGIT_WIDTH+(9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= minutes_H; //pixel en el que empieza el primer digito de minutos.
+          end
+          
+          (125+2*DIGIT_WIDTH+(9*DIGIT_WIDTH)/20) :
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= minutes_L; //pixel en el que empieza el segundo digito de minutos.
+          end
+          
+          (125+3*DIGIT_WIDTH+(9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= DRAWING_DOTS; //pixel en el que empiezan los segundos puntos.
+          end
+           
+          (125+3*DIGIT_WIDTH+(2*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= seconds_H; //pixel en el que empieza el primer digito de segundos.
+          end
+           
+          (125+4*DIGIT_WIDTH+(2*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= seconds_L; //pixel en el que empieza el segundo digito de segundos.
+          end
+           
+          (125+5*DIGIT_WIDTH+(2*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= DRAWING_DOTS; //pixel en el que empuezan los terceros puntos.
+          end
+           
+          (125+5*DIGIT_WIDTH+(3*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= milliseconds_H; //pixel en el que empieza el primer digito de milisegundos. 
+          end
+           
+          (125+6*DIGIT_WIDTH+(3*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= milliseconds_M; //pixel en el que empieza el segundo digito de milisegundos.
+          end
+           
+          (125+7*DIGIT_WIDTH+(3*9*DIGIT_WIDTH)/20):
+          begin
+            counter_row = 0;
+            counter_col = 0;
+            state <= milliseconds_L; //pixel en el que empieza el tercer digito de milisegundos.
+
+          end
+           
+          default: state <= state;
+    
+        endcase
+      end
+
+      case (state) //Switch donde se dibuja el simbolo correspondiente.
+        DRAWING_0 : Draw0;
+        DRAWING_1 : Draw1;
+        DRAWING_2 : Draw2;
+        DRAWING_3 : Draw3;
+        DRAWING_4 : Draw4;
+        DRAWING_5 : Draw5;
+        DRAWING_6 : Draw6;
+        DRAWING_7 : Draw7;
+        DRAWING_8 : Draw8;
+        DRAWING_9 : Draw9;
+        DRAWING_DOTS : DrawDots;
+       
+        default: 
+      endcase
+      
+      //switch donde se elige que dibujar dependiendo el estado.
 		 end
 		 else
 			g<=OFF;
 
 	  end
 	end
-    //Actualizo el digito correspondiente a las horas
-    always @(hours) begin
-      pos_i= 0;
-      Clear;
-      case (hours)
-       0 : Draw0;
-       1 : Draw1;
-       2 : Draw2;
-       3 : Draw3;
-       4 : Draw4;
-       5 : Draw5;
-       6 : Draw6;
-       7 : Draw7;
-       8 : Draw8;
-       9 : Draw9; 
-      endcase
-    end
+    
     //Actualizo los digitos correspondientes a los minutos
     always @(minutes) begin
-      pos_i= (DIGIT_WIDTH*29)/20;
       minutes_H = minutes/10;
       minutes_L = minutes%10;
-      Clear;
-      case (minutes_H)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-      endcase
-
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Clear;
-
-      case (minutes_L)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-        6 : Draw6;
-        7 : Draw7;
-        8 : Draw8;
-        9 : Draw9;
-      endcase
     end
     //Actualizo los digitos correspondientes a los segundos
     always @(seconds) begin
-      pos_i= DIGIT_WIDTH*4;
       seconds_H= seconds/10;
       seconds_L= seconds%10;
-      Clear;
-
-      case (seconds_H)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-      endcase
-
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Clear;
-
-      case (seconds_L)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-        6 : Draw6;
-        7 : Draw7;
-        8 : Draw8;
-        9 : Draw9;
-      endcase
     end
     //Actualizo los digitos correspondientes a los milisegundos
     always @(milliseconds) begin
-      pos_i= (DIGIT_WIDTH*131)/20;
       milliseconds_H= milliseconds/100;
       milliseconds_M= (milliseconds%100)/10;
       milliseconds_L= milliseconds%10;
-      Clear;
-
-      case (milliseconds_H)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-        6 : Draw6;
-        7 : Draw7;
-        8 : Draw8;
-        9 : Draw9;
-      endcase
-
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Clear;
-
-      case (milliseconds_M)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-        6 : Draw6;
-        7 : Draw7;
-        8 : Draw8;
-        9 : Draw9;
-      endcase
-
-      pos_i=pos_i +(DIGIT_WIDTH/10); //dejo un espacio
-      Clear;
-
-      case (milliseconds_L)
-        0 : Draw0;
-        1 : Draw1;
-        2 : Draw2;
-        3 : Draw3;
-        4 : Draw4;
-        5 : Draw5;
-        6 : Draw6;
-        7 : Draw7;
-        8 : Draw8;
-        9 : Draw9;
-      endcase
-
     end
 endmodule // ImageDrawerhour
