f995c78cc382b95e5d95c1afb76cc55aab96d48d
Ezequiel <evijande@itba.edu.ar>
2018-11-19 22:19:29 -0300

    Agregue modulo que pasa clk de 50k a 1k

---
+++ Verilog/PseudoPll/src/PseudoPll.v
@@ -0,0 +1,25 @@
+//Modulo que recibe como entrada un clock de 50KHz
+//y devuelve un clock de 1KHz
+module PseudoPll(
+  clk_in,
+  clk_out
+);
+input wire clk_in;
+output reg clk_out;
+reg [7:0] counter;
+initial begin
+  counter=0;
+  clk_out=0;
+end
+always @(posedge clk_in) begin
+  if (counter < (25-1)) begin //Aumenta el contador con cada flanco ascendente
+    counter <= counter +1;
+  end
+  else begin
+    counter <=0;
+    clk_out <= ~clk_out;
+  end
+
+end
+
+endmodule // PseudoPll
